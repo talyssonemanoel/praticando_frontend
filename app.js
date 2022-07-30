3312bad2959dacd3e1c95194aedf7090358490fd
talyssonemanoel <90127158+talyssonemanoel@users.noreply.github.com>
2022-07-30 13:16:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+//let button = document.getElementById('button');
+//let input1 = document.getElementById('input1');
+//let input2 = document.getElementById('input2');
+
+/*function somar(numero1, numero2)
+{
+    if (typeof numero1 === 'number' && typeof numero2 === 'number')
+    {
+        return numero1 + numero2
+    }
+    else
+    {
+        return Number(numero1) + Number(numero2)
+    }
+}
+
+button.addEventListener('click', () => {
+    console.log(somar(input1.value, input2.value));
+})*/
